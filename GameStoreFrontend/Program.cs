9ff74aafa1ecf1ee6a3bf49399bc0bf35a6035de
leoestevves <leogoncalves60@gmail.com>
2024-07-01 14:41:01 -0300

    Dependency injection

--- GameStoreFrontend/Program.cs
+++ GameStoreFrontend/Program.cs
@@ -1,9 +1,12 @@
+using GameStoreFrontend.Clients;
 using GameStoreFrontend.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents();
+builder.Services.AddSingleton<GamesClient>();
+builder.Services.AddSingleton<GenresClient>();
 
 var app = builder.Build();
 
